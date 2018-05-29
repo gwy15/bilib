9c734d595afbfc04f4e553342c455c769e825b6b
gwy15 <gwy15thu@gmail.com>
2018-05-29 20:40:58 +0800

    update test

--- tests/config.py
+++ tests/config.py
@@ -7,4 +7,16 @@ def getConfig():
     if not os.path.exists(configFile):
         raise RuntimeError('测试用配置文件未找到')
     with open(configFile) as f:
-        return json.load(f)
+        result = json.load(f)
+
+    userFile = os.path.dirname(__file__) + os.sep + 'user_account.json'
+    if os.path.exists(userFile):
+        with open(userFile) as f:
+            result.update(json.load(f))
+    else:
+        for key in ('phone', 'username', 'password'):
+            if not key in os.environ:
+                raise RuntimeError('未找到环境变量 %s' % key)
+            result[key] = os.environ[key]
+
+    return result
