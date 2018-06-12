9a34915f8bb61ffa7417edcb5ce55b6bdece0978
gwy15 <gwy15thu@gmail.com>
2018-06-12 14:19:43 +0800

    调整测试中的 logging

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
+import logging
 import unittest
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.ERROR)
     discover = unittest.defaultTestLoader.discover(
         'tests', pattern='test*.py')
     runner = unittest.TextTestRunner()
